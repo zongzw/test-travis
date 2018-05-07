3feb26eea15fb5314d2abf4215cf01540a03aed2
zongzhaowei <bjzongzw@cn.ibm.com>
2018-05-07 21:24:13 +0800

    Update docker file and main.go

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,9 @@
-FROM golang:1.8
+FROM scratch
 
 #Copy VSI binary to contanier
-COPY vsi /root/vsi
-RUN mkdir /var/log/vsi
-
-#start VSI app
-ENTRYPOINT /root/vsi --config=/etc/config/config.json
+COPY test-travis /test
 
 #listen port 8080
 EXPOSE 8080
+
+CMD ["/test"]
--- main.go
+++ main.go
@@ -2,15 +2,18 @@ package main
 
 import (
 	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	fmt.Println("hello world")
-	//router = gin.Default()
+	router = gin.Default()
 
-	//router.GET("/ping", func(c *gin.Context) {
-	//	c.JSON(http.StatusOK, gin.H{"message": "OK"})
-	//})
+	router.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"message": "OK"})
+	})
 
-	//router.Run()
+	router.Run("0.0.0.0:8080")
 }
